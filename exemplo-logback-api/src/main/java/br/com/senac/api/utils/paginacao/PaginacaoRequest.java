10ca200c99238e29b009c00f3233c20424eb6126
ERICK DA ROSA BERGER <erick.berger@alunos.sc.senac.br>
2024-09-17 20:01:54 -0300

    primeiro commit

---
+++ exemplo-logback-api/src/main/java/br/com/senac/api/utils/paginacao/PaginacaoRequest.java
@@ -0,0 +1,31 @@
+package br.com.senac.api.utils.paginacao;
+
+public class PaginacaoRequest {
+    private int pagina;
+    private int tamanho;
+    private String orderBy;
+
+    public int getPagina() {
+        return pagina;
+    }
+
+    public void setPagina(int pagina) {
+        this.pagina = pagina;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getOrderBy() {
+        return orderBy;
+    }
+
+    public void setOrderBy(String orderBy) {
+        this.orderBy = orderBy;
+    }
+}
